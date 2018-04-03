55c29e3bc96651692da1715b07307cb28da2ac5d
Zhaeong <owen_zzz@hotmail.com>
2018-04-02 23:50:18 -0400

    Using CodeBlocks IDE, setted up basic files

---
+++ Common/CommonFunctions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+int LoadShaders(char *vertexShaderSource, char *fragmentShaderSource);
---
+++ CommonFunctions.cpp
@@ -0,0 +1,49 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+
+int LoadShaders(char *vertexShaderSource, char *fragmentShaderSource)
+{
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	std::cout << "heyheyhey\n";
+
+	return shaderProgram;
+
+
+
+
+}
